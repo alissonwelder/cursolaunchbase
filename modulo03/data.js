3f6c09f3fa589ebc84fc478b2cb5f73c181adc27
Alisson Welder <alissonwelders@gmail.com>
2020-02-19 22:09:23 -0300

    data.js

---
+++ modulo03/data.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulario animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável this no Javascript | Mayk Brito",
+        duration: "48 min",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API? REST e RESTful? | Mayk Brito",
+        duration: "34 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "37 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: Higher Order Functions | Mayk Brito",
+        duration: "55 min",
+        price: "Free"
+        
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+        duration: "34 min",
+        price: "Free"
+        
+    },
+    
+    
+
+]
--- modulo03/server.js
+++ modulo03/server.js
@@ -2,22 +2,36 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 
 server.get("/", function(req, res){
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/49004112?s=400&u=4a50ac2b7bd4c455544ecd397fd109103c2583f5&v=4",
+        name: "Alisson Welder",
+        role: "Instrutor Web Senior",
+        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Acompanhe meu <a href="http://www.github.com/alissonwelder" target="_blank">GitHub</a>',
+        links: [
+            { name: "Facebook", url: "http://facebook.com/alissonwelder"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/alisson-welder-288327169/"}
+        ]
+    }
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio")
+    return res.render("portfolio", {items: videos})
 })
 
 server.listen(5000, function(){
