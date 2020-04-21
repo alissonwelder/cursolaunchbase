324f14d8ce5faae6aaa92f21c24ceeab5a7f909b
Alisson Welder <alissonwelders@gmail.com>
2020-04-21 15:14:21 -0300

    metodo put

--- modulo04/instructor.js
+++ modulo04/instructor.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const data = require("./data.json")
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 const Intl = require('intl')
+
 
 //show
 exports.show = function(req, res){
@@ -66,14 +67,42 @@ exports.post = function(req, res){
 // edit
 exports.edit = function(req, res){
     const { id } = req.params
-
+    
     const foundInstructor = data.instructors.find(function(instructor){
         return instructor.id == id
     })
 
     if (!foundInstructor) return res.send("instructor not found!")
+
     
-    return res.render('instructors/edit', {instructor: foundInstructor})
+    return res.render('instructors/edit', {instructor: foundInstructor })
 }
 
-// delete
+// put
+exports.put = function (req, res){
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if (id == instructor.id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send("instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
